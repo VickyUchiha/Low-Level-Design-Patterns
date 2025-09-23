30a0ab13c486cb70fcf538095e50c3c0b41ef594
VickyUchiha <vignesh561997@gmail.com>
2025-09-23 22:03:44 +0530

    Feat : Folder Structure

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
-﻿using DPatterns.Adapter;
-using DPatterns.Adapter.Adaptee;
-using DPatterns.Adapter.Target;
-using DPatterns.Chain_of_Responsibility;
-using DPatterns.Chain_of_Responsibility.ConcreteHandlers;
+﻿using System;
 
 namespace DPatterns
 {
@@ -10,25 +6,8 @@ namespace DPatterns
     {
         static void Main(string[] args)
         {
-            ////  ADAPTER PATTERN 
-
-            //IPaymentInterface payment = new PaymentAdapter(new PayPalPayment());
-            //payment.Pay(10);
-            ////******************************************************************************************************************************************************
-
-            //CHAIN OF RESPONSIBILITY
-
-            //Handler support = new SupportAgent();
-            //Handler teamLead = new TeamLead();
-            //Handler manager = new Manager();
-
-            ////Create a chain (agent -> teamLead -> manager)
-            //support.setNext(teamLead);
-            //teamLead.setNext(manager);
-
-            //support.HandleRequest(1);
-            //support.HandleRequest(2);
-            //support.HandleRequest(3);
+            Console.WriteLine("Design Patterns in C#");
+            
         }
     }
 }
